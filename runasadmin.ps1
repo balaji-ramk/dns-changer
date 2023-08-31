c3f4332550bd5388ff645597150d69385a3e2a2a
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 21:59:21 +0530

    changes

--- runasadmin.ps1
+++ runasadmin.ps1
@@ -2,11 +2,11 @@
 Set-Location Downloads
 # $currentdir = [System.Environment]::CurrentDirectory
 $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
-# $tempdir = $currentdir + "\dnschanger_temp"
-# $filepath = $currentdir + "\dnschanger_temp\changedns.ps1"
+$filepath = [System.Environment]::CurrentDirectory + "\dnschanger_temp\changedns.ps1"
 
 #commands
 # New-Item -itemtype Directory -Path dnschanger_temp
 Invoke-WebRequest $DownloadURL -OutFile "changedns.ps1"
-Start-Process PowerShell -Verb RunAs -ArgumentList ".\changedns.ps1"
+Start-Process PowerShell -Verb RunAs -ArgumentList $filepath
+Write-Output $filepath
 # Remove-Item -Force -Recurse dnschanger_temp
