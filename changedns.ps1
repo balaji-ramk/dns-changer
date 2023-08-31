ecc12ec4199adcb443ec9af5622eaa90234c50cc
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 19:30:38 +0530

    test

--- changedns.ps1
+++ changedns.ps1
@@ -1,5 +1,6 @@
 Write-Output "Changing DNS"
 Set-DNSClientServerAddress * -ServerAddresses ("1.1.1.1","1.0.0.1","2606:4700:4700::1111","2606:4700:4700::1001")
 Write-Output DNS Servers Changed System-wide!
-Remove-Item [System.Environment]::CurrentDirectory + "\stuff\changedns.ps1"
+$test = [System.Environment]::CurrentDirectory + "\stuff"
+Remove-Item $test
 Read-Host -Prompt "Press Enter to exit"
