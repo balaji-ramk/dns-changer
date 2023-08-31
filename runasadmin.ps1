676766aaad73942864d22564400de554bb3122d9
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 20:41:55 +0530

    trying something

--- runasadmin.ps1
+++ runasadmin.ps1
@@ -1,13 +1,10 @@
 #variables
-#$currentdir = [System.Environment]::CurrentDirectory
-#$DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
-#$tempdir = $currentdir + "\Downloads\dns-changer-temp\"
-#$filepath = $tempdir + "changedns.ps1"
+$currentdir = [System.Environment]::CurrentDirectory
+$DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
+$filepath = $currentdir + "\dns-changer-temp\changedns.ps1"
 
 #commands
 New-Item -itemtype Directory -Path dns-changer-temp
-#Write-Output $tempdir
-#Write-Output $filepath
 Invoke-WebRequest $DownloadURL -outfile "dns-changer-temp\changedns.ps1"
-Start-Process PowerShell -Verb RunAs -ArgumentList "dns-changer-temp\changedns.ps1"
-# Remove-Item -Force -Recurse $tempdir
+Start-Process PowerShell -Verb RunAs -ArgumentList $filepath
+Remove-Item -Force -Recurse dns-changer-temp
