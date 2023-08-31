b3a4548f1dfa1f1452961e7e750554cedfafb770
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 19:00:06 +0530

    cringe2

--- runasadmin.ps1
+++ runasadmin.ps1
@@ -1,4 +1,6 @@
-$DownloadURL = "C:\Users\bala\Documents\code\dns-changer\dns-changer\changedns.ps1"
-$response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
-
-Start-Process PowerShell -Verb RunAs -ArgumentList 
+mkdir %userprofile%\Downloads\stuff
+$DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
+Invoke-WebRequest $DownloadURL -outfile %userprofile%\Downloads\stuff\changedns.ps1
+# $response = "C:\Users\bala\Documents\code\dns-changer\dns-changer\changedns.ps1"
+# $respcont = $response.Content
+Start-Process PowerShell -Verb RunAs -ArgumentList "%userprofile%\Downloads\stuff\changedns.ps1"
