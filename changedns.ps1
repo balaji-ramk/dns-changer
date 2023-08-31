ebf1fc4b9e4a41f35f2f24cc4eb13690f4f32a8f
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 19:19:15 +0530

    cringe3

--- changedns.ps1
+++ changedns.ps1
@@ -3,5 +3,5 @@ Write-Output "Changing DNS"
 Set-DNSClientServerAddress * -ServerAddresses ("1.1.1.1","1.0.0.1","2606:4700:4700::1111","2606:4700:4700::1001")
 Write-Output DNS Servers Changed System-wide!
 # cmd /c pause
-Remove-Item "%userprofile%\Downloads\stuff\changedns.ps1"
+# Remove-Item "%userprofile%\Downloads\stuff\changedns.ps1"
 Read-Host -Prompt "Press Enter to exit"
--- runasadmin.ps1
+++ runasadmin.ps1
@@ -1,6 +1,8 @@
-mkdir %userprofile%\Downloads\stuff
+mkdir stuff
 $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
-Invoke-WebRequest $DownloadURL -outfile %userprofile%\Downloads\stuff\changedns.ps1
+# Invoke-WebRequest $DownloadURL -outfile %userprofile%\Downloads\stuff\changedns.ps1
+# $response = Invoke-WebRequest $DownloadURL
+Invoke-WebRequest $DownloadURL -outfile stuff\changedns.ps1
 # $response = "C:\Users\bala\Documents\code\dns-changer\dns-changer\changedns.ps1"
 # $respcont = $response.Content
-Start-Process PowerShell -Verb RunAs -ArgumentList "%userprofile%\Downloads\stuff\changedns.ps1"
+Start-Process PowerShell -Verb RunAs -ArgumentList "stuff\changedns.ps1"
