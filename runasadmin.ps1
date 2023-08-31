1d0ab672c11994f7a779a4eddc5f34f1850d8b53
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 19:50:12 +0530

    testing why not work

--- runasadmin.ps1
+++ runasadmin.ps1
@@ -1,10 +1,10 @@
 $currentdir = [System.Environment]::CurrentDirectory
 $tempdir = $currentdir + "\stuff"
 $filepath = $currentdir + "\stuff\changedns.ps1"
-
-mkdir $tempdir
-
 $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
+mkdir $tempdir
+echo $tempdir
+echo $filepath
 Invoke-WebRequest $DownloadURL -outfile $filepath
 Start-Process PowerShell -Verb RunAs -ArgumentList $filepath
 Remove-Item -Force -Recurse $tempdir
