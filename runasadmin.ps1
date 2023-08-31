a235306e47591fafc88ea12764de5bc1ffe4ee74
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 19:57:01 +0530

    minor shit

--- runasadmin.ps1
+++ runasadmin.ps1
@@ -1,10 +1,10 @@
 $currentdir = [System.Environment]::CurrentDirectory
-$tempdir = $currentdir + "\stuff"
-$filepath = $currentdir + "\stuff\changedns.ps1"
 $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
+$tempdir = $currentdir + "\stuff\"
+$filepath = $currentdir + "\stuff\changedns.ps1"
 mkdir $tempdir
-echo $tempdir
-echo $filepath
+Write-Output $tempdir
+Write-Output $filepath
 Invoke-WebRequest $DownloadURL -outfile $filepath
 Start-Process PowerShell -Verb RunAs -ArgumentList $filepath
 Remove-Item -Force -Recurse $tempdir
