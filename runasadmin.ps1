7f5c19f35fc3fa19c11ee24dc37735f1452da8cf
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 20:44:22 +0530

    trying something

--- runasadmin.ps1
+++ runasadmin.ps1
@@ -5,6 +5,6 @@ $filepath = $currentdir + "\dns-changer-temp\changedns.ps1"
 
 #commands
 New-Item -itemtype Directory -Path dns-changer-temp
-Invoke-WebRequest $DownloadURL -outfile "dns-changer-temp\changedns.ps1"
+Invoke-WebRequest $DownloadURL -outfile "\dns-changer-temp\changedns.ps1"
 Start-Process PowerShell -Verb RunAs -ArgumentList $filepath
 Remove-Item -Force -Recurse dns-changer-temp
