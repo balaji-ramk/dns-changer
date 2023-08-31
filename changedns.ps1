9a4c448f49cc7bd63179ab6071c38a64853e1f2f
balaji-ramk <balaji.ramk@proton.me>
2023-08-31 18:28:10 +0530

    hehe

--- changedns.ps1
+++ changedns.ps1
@@ -2,3 +2,5 @@
 Write-Output "Changing DNS"
 Set-DNSClientServerAddress * -ServerAddresses ("1.1.1.1","1.0.0.1","2606:4700:4700::1111","2606:4700:4700::1001")
 Write-Output DNS Servers Changed System-wide!
+# cmd /c pause
+Read-Host -Prompt "Press Enter to exit"
--- runasadmin.ps1
+++ runasadmin.ps1
@@ -1,4 +1,4 @@
-# $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/changedns.ps1"
-# $response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
+$DownloadURL = "C:\Users\bala\Documents\code\dns-changer\dns-changer\changedns.ps1"
+$response = Invoke-WebRequest -Uri $DownloadURL -UseBasicParsing
 
-Start-Process PowerShell -Verb RunAs -ArgumentList "C:\Users\bala\Downloads\changedns.ps1"
+Start-Process PowerShell -Verb RunAs -ArgumentList 
