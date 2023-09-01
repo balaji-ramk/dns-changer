88829165dbaf30f395a59edef474f2d9c9761ede
balaji-ramk <balaji.ramk@proton.me>
2023-09-02 04:01:02 +0530

    changed all of runner

--- runner.ps1
+++ runner.ps1
@@ -1,14 +1,14 @@
-$DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/code.ps1"
-$userdir = "$env:userprofile"
-$tempdir = $userdir + "\Downloads"
-$filepath = $userdir + "\Downloads\dns-changer-temp\code.ps1"
+# $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/code.ps1"
+# $userdir = "$env:userprofile"
+# $tempdir = $userdir + "\Downloads"
+# $filepath = $userdir + "\Downloads\dns-changer-temp\code.ps1"
 
-Set-Location $tempdir
-New-Item -ItemType Directory dns-changer-temp
+# Set-Location $tempdir
+# New-Item -ItemType Directory dns-changer-temp
 
-Invoke-WebRequest $DownloadURL -OutFile $filepath
-Write-Output $filepath
-Start-Process PowerShell -Verb RunAs -ArgumentList $filepath
+# Invoke-WebRequest $DownloadURL -OutFile $filepath
+# Write-Output $filepath
+Start-Process PowerShell -Verb RunAs -ArgumentList 'iwr "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/code.ps1" -UseBasicParsing | iex'
 
-# Remove-Item -Force -Recurse dns-changer-temp
-Set-Location ..
+# # Remove-Item -Force -Recurse dns-changer-temp
+# Set-Location ..
