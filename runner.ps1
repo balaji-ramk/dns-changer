bc28de45cb2432c200a0aec44724f7d81e30d471
balaji-ramk <balaji.ramk@proton.me>
2023-09-01 22:25:15 +0530

    changes

--- runner.ps1
+++ runner.ps1
@@ -1,7 +1,8 @@
 $DownloadURL = "https://raw.githubusercontent.com/balaji-ramk/dns-changer/main/code.ps1"
-$filepath = "$env:userprofile\Downloads\dns-changer-temp\code.ps1"
+$userdir = "$env:userprofile"
+$filepath = $userdir + "\Downloads\dns-changer-temp\code.ps1"
 
-Set-Location $env:userprofile\Downloads
+Set-Location $userdir + "\Downloads"
 mkdir dns-changer-temp
 
 Invoke-WebRequest $DownloadURL -OutFile $filepath
